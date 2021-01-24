bed7b989c033f11358a35a74909ddf8a2c086d65
tytao <1522014784@qq.com>
2021-01-24 22:07:58 +0800

    add set cookie

--- src/main/java/com/tytao/community/controller/AuthorizeController.java
+++ src/main/java/com/tytao/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(Client_id);
         accessTokenDTO.setClient_secret(Client_secret);
@@ -50,12 +53,14 @@ public class AuthorizeController {
 
         if (githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token", token));
             // 登录成功，写cookie， session
             request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
--- src/main/java/com/tytao/community/mapper/UserMapper.java
+++ src/main/java/com/tytao/community/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package com.tytao.community.mapper;
 import com.tytao.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(accountid, token, name,gmt_create, gmt_modified)" +
             "values (#{accountId},#{token},#{name},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(String token);
 }
