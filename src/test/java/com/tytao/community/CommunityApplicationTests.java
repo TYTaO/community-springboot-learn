7a07f5b0d4ea1e8bdb7061725b46aafef17f7b8a
tytao <1522014784@qq.com>
2021-02-03 15:57:13 +0800

    fix test

--- src/test/java/com/tytao/community/CommunityApplicationTests.java
+++ src/test/java/com/tytao/community/CommunityApplicationTests.java
@@ -1,31 +1,15 @@
 package com.tytao.community;
 
-import com.tytao.community.enums.CommentTypeEnum;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void testEnum(){
-        Integer type = CommentTypeEnum.QUESTION.getType();
-        System.out.println(type);
-    }
-
-    @Test
-    void testSplits(){
-        String[] s = StringUtils.split("aaa aa  b", " ");
-        for (String s1 : s) {
-            System.out.println(s1);
-        }
-    }
-
-
-
 }
